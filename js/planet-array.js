a0f2513e71a6162aa6d2b2b3010051950d7e0ded
Amberlee <amberleec12@yahoo.com>
2017-04-13 09:27:18 -0500

    my initial commit

--- js/planet-array.js
+++ js/planet-array.js
@@ -6,7 +6,8 @@
     "use strict";
 
     // TODO: Create an array holding the names of the eight planets in our solar system in order, starting closest to the sun.
-    var planets = [];
+    var planets = ["Mars", "Venus", "Pluto", "Earth", "Jupiter", "Saturn", "Neptune"
+    ];
 
     // function for logging the planets array
     function logPlanets() {
@@ -19,36 +20,47 @@
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
     // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+   planets.unshift("The Sun")
     logPlanets();
 
     console.log('Adding "Pluto" to the end of the planets array.');
     // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    planets.unshift("Saturn")
     logPlanets();
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
     // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    planets.unshift("Neptune")
     logPlanets();
 
     console.log('Removing "Pluto" from the end of the planets array.');
     // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    planets.unshift("Jupiter")
     logPlanets();
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
     // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    var planetAfterEarth = indexOfEarth +1;
+    planets.splice(planetAfterEarth, 1)
+
 
     console.log('Using splice to remove the planet after "Earth".');
     // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    planets.unshift("Pluto")
     logPlanets();
 
     console.log('Using splice to add back the planet after "Earth".');
     // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    planets.unshift("Venus")
     logPlanets();
 
     console.log("Reversing the order of the planets array.");
     // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    planets.unshift("Mars")
     logPlanets();
 
     console.log("Sorting the planets array.");
     // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    planets.unshift("Earth")
     logPlanets();
 })();
