a03e5af5b635a403f768fa9860026912c5c59107
Amberlee <amberleec12@yahoo.com>
2017-04-14 09:49:22 -0500

    my initial commit

--- js/planet-string.js
+++ js/planet-string.js
@@ -16,7 +16,7 @@
     console.log(planetsArray);
 
     // TODO:        9 Create a string with <br> tags between each planet. console.log() your results.
-    //       Why might this be useful?.
+    //       Why might this be useful?
     var planetsArray.join(<br>)
     // Bonus: Create a second string that would display your planets in an undordered list.
     //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
